dc63c45531e62ead206bc45c25aa7a5ed8048efd
Kaleeth <gregor.16.waw@gmail.com>
2022-03-30 19:00:42 +0200

    cmake linkage

---
+++ Trivia/linkage/cmake/src/main_file.cpp
@@ -0,0 +1,11 @@
+#include <cassert>
+
+#include "header_file.h"
+
+int main() {
+    assert(&Pi == get_pi());
+    assert(Pi == 3);
+    
+    assert(&get_PI == get_f());
+    assert(get_PI() == 3);
+}
