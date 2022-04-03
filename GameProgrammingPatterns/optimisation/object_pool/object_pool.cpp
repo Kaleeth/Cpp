8d3e8bddce444a1ceff408e9739e7fea90adc11d
Kaleeth <gregor.16.waw@gmail.com>
2022-04-03 16:11:02 +0200

    object pool

--- GameProgrammingPatterns/optimisation/object_pool/object_pool.cpp
+++ GameProgrammingPatterns/optimisation/object_pool/object_pool.cpp
@@ -1,45 +1,50 @@
+#include <cassert>
 
 class Particle {
 public:
     Particle()
-    : framesLeft(0) {}
+    : framesLeft_(0) {}
 
     void init(double x, double y, double xVel, double yVel, int lifetime) {
-        state_.live.x = x; state_.live.y = y;
-        state_.live.xVel = xVel; state_.live.yVel = yVel;
-        state_.live.framesLeft = lifetime;
+        state_.live_.x_ = x; state_.live_.y_ = y;
+        state_.live_.xVel_ = xVel; state_.live_.yVel_ = yVel;
+        framesLeft_ = lifetime;
     }
-    void animate() {
-        if (!inUse()) return;
-        state_.live.framesLeft--;
-        state_.live.x += state_.live.xVel;
-        state_.live.y += state_.live.yVel;
-    }
-    bool inUse() const { return state_.live.framesLeft > 0; }
 
-    Particle* getNext() const { return state_.next; }
-    void setNext(Particle* next) { state_.next = next; }
+    bool animate();
+
+    bool inUse() const { return framesLeft_ > 0; }
+
+    Particle* getNext() const { return state_.next_; }
+    void setNext(Particle* next) { state_.next_ = next; }
 
 private:
+    int framesLeft_;
+
     union {
         struct {
-            int framesLeft;
-            double x, y;
-            double xVel, yVel;
-        } live;
-        Particle* next;
+            double x_, y_;
+            double xVel_, yVel_;
+        } live_;
+        Particle* next_;
     } state_;
 };
+
+bool Particle::animate() {
+    if (!inUse()) return false;
+
+    framesLeft_--;
+    state_.live_.x_ += state_.live_.xVel_;
+    state_.live_.y_ += state_.live_.yVel_;
+    return framesLeft_ == 0;
+} 
 
 class ParticlePool{
 public:
     void create(double x, double y, double xVel, double yVel, int lifetime);
     
-    void animate() {
-        for (int i = 0; i < POOL_SIZE; i++) {
-            particles_[i].animate();
-        }
-    }
+    void animate();
+    
 private:
     static const int POOL_SIZE = 100;
     Particle particles_[POOL_SIZE];
@@ -55,15 +60,25 @@ ParticlePool::ParticlePool()
     particles_[i].setNext(&particles_[i + 1]);
     }
     // The last one terminates the list.
-    particles_[POOL_SIZE - 1].setNext(NULL);
+    particles_[POOL_SIZE - 1].setNext(nullptr);
 }
 
 void ParticlePool::create(double x, double y, double xVel, double yVel, int lifetime) {
-    // Find an available particle.
+    // Make sure the pool isn't full.
+    assert(firstAvailable_ != nullptr);
+
+    // Remove it from the available list.
+    Particle* newParticle = firstAvailable_;
+    firstAvailable_ = newParticle->getNext();
+    newParticle->init(x, y, xVel, yVel, lifetime);
+}
+
+void ParticlePool::animate() {
     for (int i = 0; i < POOL_SIZE; i++) {
-        if (!particles_[i].inUse()) {
-            particles_[i].init(x, y, xVel, yVel, lifetime);
-            return;
+        if (particles_[i].animate()) {
+            // Add this particle to the front of the list.
+            particles_[i].setNext(firstAvailable_);
+            firstAvailable_ = &particles_[i];
         }
     }
 }
